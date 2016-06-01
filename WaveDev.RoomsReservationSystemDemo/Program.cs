5494c2b50fec95c9a09a159d8b2af212b78edcb7
robin:sedlaczek <robin.sedlaczek@live.de>
2016-06-02 00:47:02 +0200

    Add private and business guest types.

--- WaveDev.RoomsReservationSystemDemo/Program.cs
+++ WaveDev.RoomsReservationSystemDemo/Program.cs
@@ -14,22 +14,52 @@ namespace RoomsReservationSystem
                 VerificationNumber = 111
             };
 
-            var guest = new Guest
-            {
-                Id = 1,
-                FirstName = "Max",
-                LastName = "Muster",
-                CreditCard = creditCard
-            };
+            var guest = new PrivateGuest(1, "Max", "Muster", creditCard);
             
-            if (guest is Guest("1", "Max", *))
-            {
-                var newGuest = guest with { FirstName = "Paul" };
-            }
+            //if (guest is Guest("1", "Max", *))
+            //{
+            //    var newGuest = guest with { FirstName = "Paul" };
+            //}
 
             Console.WriteLine(guest.FirstName);
             Console.WriteLine(guest.CreditCard.Number);
 
         }
+
+        private ISensitiveObject CreatePrivateGuest()
+        {
+            var creditCard = CreateCreditCard();
+            return new PrivateGuest(3, "Robin", "Sedlaczek", creditCard);
+        }
+
+        private CreditCard CreateCreditCard()
+        {
+            var creditCard = new CreditCard
+            {
+                Id = 1,
+                Number = "560580100",
+                ValidUntil = DateTime.Today.AddYears(1),
+                VerificationNumber = 111
+            };
+
+            return creditCard;
+        }
+
+        private Guest CreateBusinessGuest()
+        {
+            return new BusinessGuest(2, "Siemens", string.Empty);
+        }
+
+        private Guest CreateBusinessGuestWithCreditCard()
+        {
+            var guest = new BusinessGuest(2, "Siemens", string.Empty);
+
+            var creditCard = CreateCreditCard();
+            guest.CreditCards.Add(creditCard);
+
+            return guest;
+        }
+
+
     }
 }
